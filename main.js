1e787cf913c3379a73ec3c1a5d6d3facd899450a
Marco De Clara <marcodeclara@gmail.com>
2020-05-12 12:02:28 +0200

    Added function to send messages with ENTER

--- main.js
+++ main.js
@@ -25,59 +25,12 @@ $(document).mouseup(function(event) {
 // catch click on send button
 // nts.: the receiver sends .messages, while the user sends .answers 
 $('.send-btn').click(function() {
-    // get today's date
-    var date = new Date();
-    // write today's date as dd/mm/yyyy
-    var today = getTodayDate(date);
-    // write current time as hh:mm
-    var time = getTimeStamp(date);
-    // get text message written in send bar
-    var answer_text = $('#sendbar').val();
-    // if the message to be sent is't empty and the chat container is empty
-    if(answer_text.length != 0 && $('.chat-container').children().length == 0 ) {
-        // print today's date
-        printTodayDate(today);
-        // print user's message
-        printAnswer(answer_text);
-        // print timestamp
-        printTimeStampAnswer(time);
-        // clear message
-        $('#sendbar').val('');
-        // hide send button show mic button
-        showMic();
-    // if today's date is different from the date in the chat container and the last message in chat is a message from either the receiver or the user
-    } else if(answer_text.length != 0 && today != $('.chat-container .date:last').text() && ( $('.chat-container').children().last().hasClass('message-wrapper') || $('.chat-container').children().last().hasClass('answer-wrapper') ) ) {
-        // print today's date
-        printTodayDate(today);
-        // print user's message
-        printAnswer(answer_text);
-        // print timestamp
-        printTimeStampAnswer(time);
-        // clear message
-        $('#sendbar').val('');
-        // hide send button show mic button
-        showMic();
-    // if the last message in chat is a message from the user
-    } else if(answer_text.length != 0 && $('.chat-container').children().last().hasClass('answer-wrapper')) {
-        // print a followup message
-        printAnswerFollowUp(answer_text);
-        // print timestamp
-        printTimeStampAnswer(time);
-        // clear message
-        $('#sendbar').val('');
-        // hide send button show mic button
-        showMic();
-    // if the last message in chat is a message from the user
-    } else if(answer_text.length != 0 && $('.chat-container').children().last().hasClass('message-wrapper')) {
-        // print user's message
-        printAnswer(answer_text);
-        // print timestamp
-        printTimeStampAnswer(time);
-        // clear message
-        $('#sendbar').val('');
-        // hide send button show mic button
-        showMic();
-    }
+    sendMessageWithSendBtn();
+});
+
+// catch keypress enter while sendbar is focused
+$('.send-bar').keypress(function(event) {
+    sendMessageWithEnter(event);
 });
 
 // hide send button and show mic button 
@@ -146,4 +99,124 @@ function printAnswerFollowUp(answer_text) {
     var answer_label = $('#template .answer').clone().appendTo('.chat-container .answer-wrapper:last-child');
 
     $('.chat-container .answer-wrapper:last-child .answer:last-child').append(answer_text);
+};
+
+// function that sends messages when send button is pressed
+function sendMessageWithSendBtn() {
+    // get today's date
+    var date = new Date();
+    // write today's date as dd/mm/yyyy
+    var today = getTodayDate(date);
+    // write current time as hh:mm
+    var time = getTimeStamp(date);
+    // get text message written in send bar
+    var answer_text = $('#sendbar').val();
+    // if the message to be sent is't empty 
+    if(answer_text.length != 0) {
+        // if the chat container is empty
+        if($('.chat-container').children().length == 0 ) {
+            // print today's date
+            printTodayDate(today);
+            // print user's message
+            printAnswer(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        // if today's date is different from the date in the chat container and the last message in chat is a message from either the receiver or the user
+        } else if(today != $('.chat-container .date:last').text() && ( $('.chat-container').children().last().hasClass('message-wrapper') || $('.chat-container').children().last().hasClass('answer-wrapper') ) ) {
+            // print today's date
+            printTodayDate(today);
+            // print user's message
+            printAnswer(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        // if the last message in chat is a message from the user
+        } else if($('.chat-container').children().last().hasClass('answer-wrapper')) {
+            // print a followup message
+            printAnswerFollowUp(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        // if the last message in chat is a message from the user
+        } else if($('.chat-container').children().last().hasClass('message-wrapper')) {
+            // print user's message
+            printAnswer(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        }
+    }
+};
+
+// function that sends messages when ENTER is pressed
+function sendMessageWithEnter(event) {
+    // get today's date
+    var date = new Date();
+    // write today's date as dd/mm/yyyy
+    var today = getTodayDate(date);
+    // write current time as hh:mm
+    var time = getTimeStamp(date);
+    // get text message written in send bar
+    var answer_text = $('#sendbar').val();
+    // if the message to be sent is't empty 
+    if(answer_text.length != 0 && event.which == 13) {
+        //if the chat container is empty
+        if($('.chat-container').children().length == 0 ) {
+            // print today's date
+            printTodayDate(today);
+            // print user's message
+            printAnswer(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        // if today's date is different from the date in the chat container and the last message in chat is a message from either the receiver or the user
+        } else if(today != $('.chat-container .date:last').text() && ( $('.chat-container').children().last().hasClass('message-wrapper') || $('.chat-container').children().last().hasClass('answer-wrapper') ) ) {
+            // print today's date
+            printTodayDate(today);
+            // print user's message
+            printAnswer(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        // if the last message in chat is a message from the user
+        } else if($('.chat-container').children().last().hasClass('answer-wrapper')) {
+            // print a followup message
+            printAnswerFollowUp(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        // if the last message in chat is a message from the user
+        } else if($('.chat-container').children().last().hasClass('message-wrapper')) {
+            // print user's message
+            printAnswer(answer_text);
+            // print timestamp
+            printTimeStampAnswer(time);
+            // clear message
+            $('#sendbar').val('');
+            // hide send button show mic button
+            showMic();
+        }
+    }
 };
