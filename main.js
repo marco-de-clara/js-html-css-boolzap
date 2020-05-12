035ed20420f5b21dcf695acf1b0de2eba080b5c9
Marco De Clara <marcodeclara@gmail.com>
2020-05-12 10:17:51 +0200

    Fixed date and timestamp, clear message after it has been sent

--- main.js
+++ main.js
@@ -6,33 +6,30 @@ $('#notice').click(function() {
     $('.log-box').toggleClass('longer');
 });
 
-// catch click on send bar to switch from mic button to paper plane button
+// catch click on send bar to switch from mic button to paper send button
 $('#sendbar').click(function() {
-    // display mic button => none;
-    $('.voice-btn').removeClass('show');
-    // display paper plane button => inline-block;
-    $('.send-btn').addClass('show');
+    // hide mic button and show send button
+    showSend();
 });
 
-// catch click on the document and reverse display for paper plane button and mic button
+// catch click on the document and reverse display for send button and mic button
 $(document).mouseup(function(event) {
     // if the target isn't:
     // the send bar, nor the send button, nor one of their descendants
     if( !$('.send-bar').is(event.target) && !$('.send-btn').is(event.target) && $('.send-bar').has(event.target).length == 0 && $('.send-btn').has(event.target).length == 0) {
-        // show mic button
-        $('.voice-btn').addClass('show');
-        // hide paper plane button
-        $('.send-btn').removeClass('show');
+        // hide send button and show mic button
+        showMic();
     }
 });
 
-// catch click on paper plane button
+// catch click on send button
 // nts.: the receiver sends .messages, while the user sends .answers 
 $('.send-btn').click(function() {
     // get today's date
     var date = new Date();
-    // write today's date as gg/mm/yyyy
+    // write today's date as dd/mm/yyyy
     var today = getTodayDate(date);
+    // write current time as hh:mm
     var time = getTimeStamp(date);
     // get text message written in send bar
     var answer_text = $('#sendbar').val();
@@ -42,38 +39,74 @@ $('.send-btn').click(function() {
         printTodayDate(today);
         // print user's message
         printAnswer(answer_text);
+        // print timestamp
         printTimeStampAnswer(time);
+        // clear message
+        $('#sendbar').val('');
+        // hide send button show mic button
+        showMic();
     // if today's date is different from the date in the chat container and the last message in chat is a message from either the receiver or the user
     } else if(answer_text != 0 && today != $('.chat-container .date:last').text() && ( $('.chat-container').children().last().hasClass('message-wrapper') || $('.chat-container').children().last().hasClass('answer-wrapper') ) ) {
         // print today's date
         printTodayDate(today);
         // print user's message
         printAnswer(answer_text);
+        // print timestamp
         printTimeStampAnswer(time);
+        // clear message
+        $('#sendbar').val('');
+        // hide send button show mic button
+        showMic();
     // if the last message in chat is a message from the user
     } else if(answer_text != 0 && $('.chat-container').children().last().hasClass('answer-wrapper')) {
         // print a followup message
         printAnswerFollowUp(answer_text);
+        // print timestamp
         printTimeStampAnswer(time);
+        // clear message
+        $('#sendbar').val('');
+        // hide send button show mic button
+        showMic();
     // if the last message in chat is a message from the user
     } else if(answer_text != 0 && $('.chat-container').children().last().hasClass('message-wrapper')) {
         // print user's message
         printAnswer(answer_text);
+        // print timestamp
         printTimeStampAnswer(time);
+        // clear message
+        $('#sendbar').val('');
+        // hide send button show mic button
+        showMic();
     }
 });
 
-// concatenate current day, month and year into gg/mm/yyyy
+// hide send button and show mic button 
+function showMic() {
+    // show mic button
+    $('.voice-btn').addClass('show');
+    // hide paper send button
+    $('.send-btn').removeClass('show');
+};
+
+// hide mic button and show send button
+function showSend() {
+    // show mic button
+    $('.voice-btn').removeClass('show');
+    // hide paper send button
+    $('.send-btn').addClass('show');
+};
+
+// concatenate current day, month and year into dd/mm/yyyy
 function getTodayDate(date) {
 
-    return (date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear());
-}
+    return date.toLocaleDateString('it-IT');
+};
 
 // concatenate current time into hh:mm
 function getTimeStamp(date) {
 
-    return (date.getHours() + ':' + date.getMinutes());
-}
+    return date.toLocaleTimeString('it-IT', {hour: '2-digit', minute: '2-digit' });
+};
 
 // print hh:mm into its label (receiver's messages)
 function printTimeStampMessage(time) {
@@ -81,7 +114,7 @@ function printTimeStampMessage(time) {
     var time_label = $('#template .timestamp').clone().appendTo('.chat-container .message-wrapper:last-child .message:last-child');
 
     time_label.text(time);
-}
+};
 
 // print hh:mm into its label (user's messages)
 function printTimeStampAnswer(time) {
@@ -89,7 +122,7 @@ function printTimeStampAnswer(time) {
     var time_label = $('#template .timestamp').clone().appendTo('.chat-container .answer-wrapper:last-child .answer:last-child');
 
     time_label.text(time);
-}
+};
 
 // print gg/mm/yyyy into its label
 function printTodayDate(today) {
@@ -97,7 +130,7 @@ function printTodayDate(today) {
     var date_label = $('#template .date').clone().appendTo('.chat-container');
 
     date_label.text(today);
-}
+};
 
 // print user's message
 function printAnswer(answer_text) {
@@ -105,7 +138,7 @@ function printAnswer(answer_text) {
     var answer_label = $('#template .answer-wrapper').clone().appendTo('.chat-container');
 
     $('.chat-container .answer-wrapper:last-child .answer').append(answer_text);
-}
+};
 
 // print a follow up message under user's message
 function printAnswerFollowUp(answer_text) {
@@ -113,4 +146,4 @@ function printAnswerFollowUp(answer_text) {
     var answer_label = $('#template .answer').clone().appendTo('.chat-container .answer-wrapper:last-child');
 
     $('.chat-container .answer-wrapper:last-child .answer:last-child').append(answer_text);
-}
+};
