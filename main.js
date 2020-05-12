ab851ba83a3b218701180790eee74e4fc5b902cc
Marco De Clara <marcodeclara@gmail.com>
2020-05-12 11:13:20 +0200

    Small adjustments

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ $('.send-btn').click(function() {
     // get text message written in send bar
     var answer_text = $('#sendbar').val();
     // if the message to be sent is't empty and the chat container is empty
-    if(answer_text != 0 && $('.chat-container').children().length == 0 ) {
+    if(answer_text.length != 0 && $('.chat-container').children().length == 0 ) {
         // print today's date
         printTodayDate(today);
         // print user's message
@@ -46,7 +46,7 @@ $('.send-btn').click(function() {
         // hide send button show mic button
         showMic();
     // if today's date is different from the date in the chat container and the last message in chat is a message from either the receiver or the user
-    } else if(answer_text != 0 && today != $('.chat-container .date:last').text() && ( $('.chat-container').children().last().hasClass('message-wrapper') || $('.chat-container').children().last().hasClass('answer-wrapper') ) ) {
+    } else if(answer_text.length != 0 && today != $('.chat-container .date:last').text() && ( $('.chat-container').children().last().hasClass('message-wrapper') || $('.chat-container').children().last().hasClass('answer-wrapper') ) ) {
         // print today's date
         printTodayDate(today);
         // print user's message
@@ -58,7 +58,7 @@ $('.send-btn').click(function() {
         // hide send button show mic button
         showMic();
     // if the last message in chat is a message from the user
-    } else if(answer_text != 0 && $('.chat-container').children().last().hasClass('answer-wrapper')) {
+    } else if(answer_text.length != 0 && $('.chat-container').children().last().hasClass('answer-wrapper')) {
         // print a followup message
         printAnswerFollowUp(answer_text);
         // print timestamp
@@ -68,7 +68,7 @@ $('.send-btn').click(function() {
         // hide send button show mic button
         showMic();
     // if the last message in chat is a message from the user
-    } else if(answer_text != 0 && $('.chat-container').children().last().hasClass('message-wrapper')) {
+    } else if(answer_text.length != 0 && $('.chat-container').children().last().hasClass('message-wrapper')) {
         // print user's message
         printAnswer(answer_text);
         // print timestamp
